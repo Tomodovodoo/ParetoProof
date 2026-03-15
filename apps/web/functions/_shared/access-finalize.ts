b56cf387ef2fb14425e9ab5e00d9aeb127648bf3
Tom de Groot <tomthegreatest04@gmail.com>
2026-03-15 11:06:24 +0100

    fix(auth): bootstrap api session after finalize

--- apps/web/functions/_shared/access-finalize.ts
+++ apps/web/functions/_shared/access-finalize.ts
@@ -121,6 +121,21 @@ function resolvePortalRedirectTarget(rawRedirectTarget: unknown, fallbackRedirec
   }
 }
 
+function buildApiSessionBootstrapUrl(requestUrl: URL, portalRedirectTarget: string) {
+  const portalUrl = new URL(portalRedirectTarget);
+  const apiBootstrapUrl = new URL(
+    "/portal/session/finalize/submit",
+    resolveApiBaseUrl(requestUrl)
+  );
+  const redirectPath = `${portalUrl.pathname}${portalUrl.search}${portalUrl.hash}` || "/";
+
+  if (redirectPath !== "/") {
+    apiBootstrapUrl.searchParams.set("redirect", redirectPath);
+  }
+
+  return apiBootstrapUrl.toString();
+}
+
 function readSetCookieHeaders(headers: Headers) {
   const cookieHeaders = headers as Headers & {
     getAll?: (name: string) => string[];
@@ -246,5 +261,8 @@ export async function handleAccessFinalize(request: Request) {
     return buildRedirectResponse(retryUrl, finalizeResponse.headers);
   }
 
-  return buildRedirectResponse(redirectTarget, finalizeResponse.headers);
+  return buildRedirectResponse(
+    buildApiSessionBootstrapUrl(requestUrl, redirectTarget),
+    finalizeResponse.headers
+  );
 }
