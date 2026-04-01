9536b1ddce55e765ce4890e76011d4d18e245b28
Tomodovodoo <tomthegreatest04@gmail.com>
2026-04-02 00:12:19 +0200

    Bind Problem 9 statement and gold artifacts

--- benchmarks/firstproof/problem9/FirstProof/Problem9/Statement.lean
+++ benchmarks/firstproof/problem9/FirstProof/Problem9/Statement.lean
@@ -2,6 +2,8 @@ import FirstProof.Problem9.Support
 
 namespace FirstProof.Problem9
 
-axiom problem9 (n : Nat) : 2 * triangular n = n * Nat.succ n
+def problem9_target (n : Nat) : Prop := 2 * triangular n = n * Nat.succ n
+
+axiom problem9 (n : Nat) : problem9_target n
 
 end FirstProof.Problem9
